ceba421f082f583463da958a1216c7d6c7241d9d
EnriqueLC <enrique199413@gmail.com>
2018-04-16 00:38:44 -0500

    CSS and functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import mainStyles from './main.css'
 
-const Index = () => {
-  return <div>Hello React!</div>
+let userName = 'EnriqueLC'
+let todos = []
+
+const addTodo = (todo) => {
+  count++
+
+  todos.push(todo)
+  renderTodoApp()
+}
+const minusTodo = (todo) => {
+  count--
+
+  todos.push(todo)
+  renderTodoApp()
+}
+const resetTodo = (todo) => {
+  count = 0
+
+  todos.push(todo)
+  renderTodoApp()
 }
 
-ReactDOM.render(<Index/>, document.getElementById('index'))
+let count = 0
+
+const renderTodoApp = () => {
+  const Index = () => {
+    return <div>
+      <button onClick={addTodo} className={mainStyles.title}>Paco</button>
+      <button onClick={minusTodo} className={mainStyles.title}>Paco</button>
+      <button onClick={resetTodo} className={mainStyles.title}>Paco</button>
+      {count}
+    </div>
+  }
+
+  ReactDOM.render(<Index/>, document.getElementById('index'))
+}
+
+renderTodoApp()
